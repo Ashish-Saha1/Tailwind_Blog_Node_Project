09009a7eec900cb985a6ed9b9c819e51e33eac8e
ashish-saha <rubelsaha1@gmail.com>
2025-04-19 23:27:43 +0600

    env problem solve

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const expressEjsLayouts = require('express-ejs-layouts');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const ejs = require('ejs')
 
+
+
+
+
+
 app.get('/', (req,res)=>{
     res.render('./Layout/main.ejs')
     //res.send("Hello")
 })
-
-
 
 app.set('view engine', 'ejs');
 app.set('layouts', './Layout/main.ejs')
 app.use(expressEjsLayouts)
 app.use(express.static('Public'))
 
-app.listen(3000, ()=>{
-    console.log("listing on Port 3000")
+
+const PORT =  process.env.PORT
+
+app.listen(PORT, ()=>{
+    console.log(`listing on PORT ${PORT}`)
 })
